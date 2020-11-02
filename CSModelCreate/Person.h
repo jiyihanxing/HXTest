c72190456793da48dee7104d817ab9690ca35659
Tc_fenghx <Tc_fenghx@yiche.com>
2020-11-02 09:52:10 +0800

    develop1-2

---
+++ CSModelCreate/Person.h
@@ -0,0 +1,18 @@
+//
+//  Person.h
+//  CSModelCreate
+//
+//  Created by cynfenghanxing on 2020/11/2.
+//  Copyright © 2020 hanxing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+//develop1-2
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CSModelCreate/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  CSModelCreate
+//
+//  Created by cynfenghanxing on 2020/11/2.
+//  Copyright © 2020 hanxing. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
